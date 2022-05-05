810dfcbd9df4be136d0319ab66414c83b0c812d7
sadmanalam142 <sadmanalam142@gmail.com>
2022-05-06 02:24:01 +0600

    getting the deta

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cqcdf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const fruitCollection = client.db("fruitsWarehouse").collection("fruits");
+
+        app.get('/fruits', async (req, res) => {
+            const query = {};
+            const cursor = fruitCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    }
+    finally{
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('i am running node')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('successfuly listening', port)
 })
